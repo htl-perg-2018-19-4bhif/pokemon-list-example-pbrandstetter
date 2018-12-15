dad99ba6a07c2b1e9e0c2a573b9f32fb72889868
Philipp Brandstetter <philipp.brandstetter57@gmail.com>
2018-12-15 13:26:42 +0100

    First try

--- app.ts
+++ app.ts
@@ -1,3 +1,22 @@
-$.getJSON('https://pokeapi.co/api/v2/pokemon/', function (data) {
-  document.append(data);
+import * as path from 'path';
+import * as express from 'express';
+
+const app = express();
+
+app.set('view engine', 'hbs');
+//app.set('views', path.join(__dirname));
+
+//$(document).ready(function () {
+  $.getJSON('https://pokeapi.co/api/v2/pokemon/', function (data) {
+    app.get('/', function (req, res) {
+      res.render('index', {
+        title: 'PokeList',
+        pokemons: [{ id: 1, desc: 'Buy food' }, { id: 2, desc: 'Homework' },
+        { id: 3, desc: 'Play video games' }]
+      });
+    });
+  //});
+
+  app.listen(3000, () => console.log('API is listening on port 3000'));
+
 });
